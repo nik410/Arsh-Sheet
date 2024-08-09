d3a2c39d38b45deeb99f61c95bdff0b5d6a59648
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-08-09 18:12:15 +0530

    Create 226. Invert Binary Tree.java

---
+++ Stack/226. Invert Binary Tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        // if(root == null)return null;
+
+        // TreeNode temp = root.left;
+        // root.left = root.right;
+        // root.right = temp;
+
+        // invertTree(root.right);
+        // invertTree(root.left);
+
+        // return root;
+
+        if(root == null)return null;
+
+        Stack<TreeNode> st = new Stack();
+        st.push(root);
+
+        while(!st.isEmpty()){
+            TreeNode node = st.pop();
+            //Swap
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if(node.right != null){
+                st.push(node.right);
+            }
+
+            if(node.left != null){
+                st.push(node.left);
+            }
+        }
+
+        return root;
+    }
+}
